c867f86de24b88ca906b217f792bda51cbdbc3c5
Dylan Sieren <dylan.sieren01@gmail.com>
2019-07-12 00:47:58 -0500

    Updates

---
+++ precache-manifest.a3ad5b85f5a21767294359473d3ea94a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19a6a2f89bd4ac7b1f7eb7bb67b37e0c",
+    "url": "/Put-A-Fork-In-It/index.html"
+  },
+  {
+    "revision": "8928f5920b43640f2ca1",
+    "url": "/Put-A-Fork-In-It/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/Put-A-Fork-In-It/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "8928f5920b43640f2ca1",
+    "url": "/Put-A-Fork-In-It/static/js/main.7a13aac7.chunk.js"
+  },
+  {
+    "revision": "8721489f0dcb513bfcac",
+    "url": "/Put-A-Fork-In-It/static/js/runtime~main.fcc168da.js"
+  }
+]);
