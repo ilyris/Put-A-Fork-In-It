09e4ee12dc803230c73917031d7b2c8a4f1100cf
Dylan Sieren <dylan.sieren01@gmail.com>
2019-07-13 18:31:26 -0500

    figured out my CROS issue, wasn't properl setting the searchfield state value

--- src/App.js
+++ src/App.js
@@ -1,4 +1,49 @@
-import React, { useState } from 'react';
+// import React, { Component } from 'react';
+// import './App.css';
+// import SearchBox from "./componets/SearchBox";
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       searchfield: '',
+//       recipes: []
+//     }
+//   }
+
+// searchChange = event => {
+//     this.setState({searchfield: event.target.value});
+//   } 
+
+// submitUsersRecipe = (event) =>  {
+//     event.preventDefault();
+//     return this.fetchApiCall();
+//   }
+// fetchApiCall = ()  => {
+//     fetch(`https://api.edamam.com/search?q=${this.state.searchfield}&app_id=4298945d&app_key=ad5979247966b4cff1201bbc13ff7be6`)
+//     .then( response =>  {
+//       return response.json();
+
+//     })
+//     .then( recipes =>  {
+//       console.log(recipes);
+//       this.setState({recipes: recipes});
+//     }
+//       );
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1 id="companyName">Put A Fork In It</h1>
+//         <SearchBox searchChange={this.searchChange} submitUsersRecipe={this.submitUsersRecipe}/>
+//       </div>
+//     );
+//   }
+
+//     }
+
+// export default App;
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import SearchBox from "./componets/SearchBox";
 
@@ -7,26 +52,23 @@ const App = () => {
   const [recipes, setRecipes] = useState([]);
   const [searchfield, setSearchField] = useState('');
 
+  
+
+  const fetchApiCall = async () => {
+    const getResponse = await fetch(`https://api.edamam.com/search?q=${searchfield}&app_id=4298945d&app_key=ad5979247966b4cff1201bbc13ff7be6`);
+    const data = await getResponse.json();
+    setRecipes(data.hits);
+  }
+
   const searchChange = event => {
-    setSearchField({searchfield: event.target.value});
+    setSearchField(event.target.value);
   } 
 
   const submitUsersRecipe = (event) =>  {
     event.preventDefault();
     fetchApiCall();
   }
-  const fetchApiCall = ()  => {
-    fetch(`https://api.edamam.com/search?q=${searchfield}&app_id=4298945d&app_key=ad5979247966b4cff1201bbc13ff7be6`)
-    .then( response =>  {
-      return response.json();
 
-    })
-    .then( recipes =>  {
-      console.log(recipes);
-      setRecipes({recipes: recipes});
-    }
-      );
-  }
     return (
       <div>
         <h1 id="companyName">Put A Fork In It</h1>
